0d219ec619c8ae9827ca89d223eb08d2761fa844
bdsqqq <igorbedesqui@gmail.com>
2020-06-16 01:02:36 -0300

    removed PROCESS.ENV usage for compatibility with heroku

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,3 @@
-import {config as configDotenv} from 'dotenv'
-import {resolve} from 'path'
-
-switch(process.env.NODE_ENV) {
-  case "development":
-    console.log("Environment is 'development'")
-    configDotenv({
-      path: resolve(__dirname, "../.env.development")
-    })
-    break
-  case "test":
-    configDotenv({
-      path: resolve(__dirname, "../.env.test")
-    })
-    break
-  // Add 'staging' and 'production' cases here as well!
-  default:
-    throw new Error(`'NODE_ENV' ${process.env.NODE_ENV} is not handled!`)
-}
-
 const throwIfNot = <T, K extends keyof T>(obj: Partial<T>, prop: K, msg?: string): T[K] => {
   if(obj[prop] === undefined || obj[prop] === null){
     throw new Error(msg || `Environment is missing variable ${prop}`)
