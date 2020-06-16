bb0bf37e8af79e11f72e8f905f18a5e5f93c3b4a
bdsqqq <igorbedesqui@gmail.com>
2020-06-15 23:08:20 -0300

    implemented routes.ts to handle requests

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,32 @@
 import express from 'express';
+import bodyParser from "body-parser";
+import dotenv from 'dotenv'
 
-const app = express();
+import './config'
+import routes from './routes';
+
+const app = express();// Allow any method from any host and log requests
+
+dotenv.config();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    if('OPTIONS' === req.method) {
+        res.sendStatus(200);
+    } else {
+        console.log(`${req.ip} ${req.method} ${req.url}`);
+        next();
+    }
+});
+
+app.use(express.json()); // Handle POST requests that come in formatted as JSON
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(routes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server now listening on ${process.env.PORT}`);
+});
 
